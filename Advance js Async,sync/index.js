060b665d1c17f92b60249d112646eb7daab0691e
Vikash <v77870717@gmail.com>
2026-09-14 16:22:56 +0530

    add advJs folder

---
+++ Advance js Async,sync/index.js
@@ -0,0 +1,70 @@
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
+
+// let startTime = Date.now()
+
+// // while(Date.now()-startTime<10000){
+
+// // }
+
+// console.log(globalThis);
+
+// <=====*****SetTimeout*****=====>
+//****setTimeout(fun1(){},milli second me time lega)
+
+// console.log("Task 1");
+// setTimeout(function cb(){
+//     console.log("Task 2");
+
+// let startTime = Date.now()
+//  while(Date.now()-startTime<10000){
+
+//  }
+// },0)
+// console.log("Task 3");
+
+// console.log("Task 1");
+
+// setTimeout(function fun1(){
+//     console.log("task2");
+// },4000)
+// setTimeout( function fun2(){
+//     console.log("task4");
+// },1000)
+// setTimeout( function fun3(){
+//     console.log("task5");
+// },2000)
+
+// console.log("task 3");
+
+// <=====****SetIntervail*****=====>
+// setInterval((),time in milisecpind)
+
+// console.log(id);
+// clearInterval()
+
+// let count = 1
+// let id = setInterval(function(){
+
+//     count++
+//     if( count > 5){
+//         clearInterval(id)//<----
+//     }
+//      console.log("hi");
+
+// },1000)
+
+// const body = document.querySelector("body");
+// let colorStr = "0123456789abcdef";
+// setInterval(()=>{
+//     let color = "";
+//     for (let i = 0; i < 6; i++) {
+//   let randomValue = Math.floor(Math.random() * colorStr.length) 
+//   color = color + colorStr[randomValue];
+// }
+// body.style.backgroundColor = `#${color}`;
+// },500)
+
+
+
